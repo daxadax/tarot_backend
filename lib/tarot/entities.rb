ec74b165c273a3c13fc8d5c3dc007d2828a35ad4
Dax <d.dax@email.com>
2014-06-24 09:12:20 +0200

    Update entities manifest file

--- lib/tarot/entities.rb
+++ lib/tarot/entities.rb
@@ -4,10 +4,10 @@ require 'tarot/entities/card'
 require 'tarot/entities/minor_arcana'
 require 'tarot/entities/major_arcana'
 require 'tarot/entities/suit'
-require 'tarot/entities/wand'
-require 'tarot/entities/pentacle'
-require 'tarot/entities/cup'
-require 'tarot/entities/sword'
+require 'tarot/entities/wands'
+require 'tarot/entities/pentacles'
+require 'tarot/entities/cups'
+require 'tarot/entities/swords'
 
 module Tarot
   module Entities
