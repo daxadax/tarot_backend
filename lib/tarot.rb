9c432b9269e8e130e7e4bffad88ae5ddba2a8753
Dax <d.dax@email.com>
2014-07-07 18:05:43 +0200

    Refactor attributes in tarot.rb

--- lib/tarot.rb
+++ lib/tarot.rb
@@ -7,8 +7,10 @@ require_relative 'tarot/services'
 require_relative 'tarot/use_cases'
 
 module Tarot
-  class CardAttributes < Struct.new(:title, :associations); end
-  class SuitAttributes < Struct.new(:element, :associations); end
+  SuitAttributes    = Struct.new(:element, :domain)
+  MinorAttributes   = Struct.new(:title, :associations)
+  MajorAttributes   = Struct.new(:title, :element, :domain, :associations)
+
 
   LAYOUTS           = {
     :three_card => 3,
@@ -16,52 +18,102 @@ module Tarot
   }
 
   SUITS = {
-    :wands      => SuitAttributes.new(:fire,   ['yang', 'instinctual']),
+    :wands      => SuitAttributes.new(:fire,   ['masculine', 'instinctual']),
     :pentacles  => SuitAttributes.new(:earth,  ['practical', 'logistical']),
-    :cups       => SuitAttributes.new(:water,  ['yin', 'emotional', 'cultural', 'social']),
+    :cups       => SuitAttributes.new(:water,  ['feminine', 'emotional', 'cultural', 'social']),
     :swords     => SuitAttributes.new(:air,    ['intellectual'])
   }
 
-  MAJOR_ARCANA      = {
-    '00' => CardAttributes.new( 'The Fool',            [] ),
-    '01' => CardAttributes.new( 'The Magician',        [] ),
-    '02' => CardAttributes.new( 'The High Priestess',  [] ),
-    '03' => CardAttributes.new( 'The Empress',         [] ),
-    '04' => CardAttributes.new( 'The Emperor',         [] ),
-    '05' => CardAttributes.new( 'The Hierophant',      [] ),
-    '06' => CardAttributes.new( 'The Lovers',          [] ),
-    '07' => CardAttributes.new( 'The Chariot',         [] ),
-    '08' => CardAttributes.new( 'Strength',            [] ),
-    '09' => CardAttributes.new( 'The Hermit',          [] ),
-    '10' => CardAttributes.new( 'The Wheel',           [] ),
-    '11' => CardAttributes.new( 'Justice',             [] ),
-    '12' => CardAttributes.new( 'The Hanged Man',      [] ),
-    '13' => CardAttributes.new( 'Death',               [] ),
-    '14' => CardAttributes.new( 'Temperance',          [] ),
-    '15' => CardAttributes.new( 'The Devil',           [] ),
-    '16' => CardAttributes.new( 'The Tower',           [] ),
-    '17' => CardAttributes.new( 'The Star',            [] ),
-    '18' => CardAttributes.new( 'The Moon',            [] ),
-    '19' => CardAttributes.new( 'The Sun',             [] ),
-    '20' => CardAttributes.new( 'Judgement',           [] ),
-    '21' => CardAttributes.new( 'The World',           [] )
+  MINOR_ARCANA      = {
+    '01' => MinorAttributes.new(
+      :ace, ['birth', 'creation', 'a gift', 'a surprise']
+    ),
+    '02' => MinorAttributes.new(
+      :two, ['balance', 'unity', 'duality', 'conflict', 'tension']
+    ),
+    '03' => MinorAttributes.new(
+      :three, []
+    ),
+    '04' => MinorAttributes.new(
+      :four, []
+    ),
+    '05' => MinorAttributes.new(
+      :five, []
+    ),
+    '06' => MinorAttributes.new(
+      :six, []
+    ),
+    '07' => MinorAttributes.new(
+      :seven, []
+    ),
+    '08' => MinorAttributes.new(
+      :eight, []
+    ),
+    '09' => MinorAttributes.new(
+      :nine, []
+    ),
+    '10' => MinorAttributes.new(
+      :ten, []
+    ),
+    '11' => MinorAttributes.new(
+      :prince, ['routine', 'movement', 'training']
+    ),
+    '12' => MinorAttributes.new(
+      :knight, ['instinctual', 'sub-conscious desire', 'behind-the-scenes', 'drive']
+    ),
+    '13' => MinorAttributes.new(
+      :queen, ['drawn attention', 'ambition', 'desire', 'deriving pleasure from']
+    ),
+    '14' => MinorAttributes.new(
+      :king, ['concentrated attention', 'synthesis', 'supervision' ]
+    )
   }
 
-  MINOR_ARCANA      = {
-    '01' => CardAttributes.new( :ace,     ['birth', 'creation', 'a gift', 'a surprise'] ),
-    '02' => CardAttributes.new( :two,     ['balance', 'unity', 'duality', 'conflict', 'tension'] ),
-    '03' => CardAttributes.new( :three,   [] ),
-    '04' => CardAttributes.new( :four,    [] ),
-    '05' => CardAttributes.new( :five,    [] ),
-    '06' => CardAttributes.new( :six,     [] ),
-    '07' => CardAttributes.new( :seven,   [] ),
-    '08' => CardAttributes.new( :eight,   [] ),
-    '09' => CardAttributes.new( :nine,    [] ),
-    '10' => CardAttributes.new( :ten,     [] ),
-    '11' => CardAttributes.new( :prince,  ['routine', 'movement', 'training'] ),
-    '12' => CardAttributes.new( :knight,  ['instinctual', 'sub-conscious desire', 'behind-the-scenes', 'drive'] ),
-    '13' => CardAttributes.new( :queen,   ['drawn attention', 'ambition', 'desire', 'deriving pleasure from'] ),
-    '14' => CardAttributes.new( :king,    ['concentrated attention', 'synthesis', 'supervision' ] ),
+  MAJOR_ARCANA      = {
+    '00' => MajorAttributes.new(
+      'The Fool', :air, [], [] ),
+    '01' => MajorAttributes.new(
+      'The Magician', :air, [], [] ),
+    '02' => MajorAttributes.new(
+      'The High Priestess', :water, [], [] ),
+    '03' => MajorAttributes.new(
+      'The Empress', :earth, [], [] ),
+    '04' => MajorAttributes.new(
+      'The Emperor', :fire, [], [] ),
+    '05' => MajorAttributes.new(
+      'The Hierophant', :earth, [], [] ),
+    '06' => MajorAttributes.new(
+      'The Lovers', :air, [], [] ),
+    '07' => MajorAttributes.new(
+      'The Chariot', :water, [], [] ),
+    '08' => MajorAttributes.new(
+      'Strength', :fire, [], [] ),
+    '09' => MajorAttributes.new(
+      'The Hermit', :earth, [], [] ),
+    '10' => MajorAttributes.new(
+      'The Wheel', :fire, [], [] ),
+    '11' => MajorAttributes.new(
+      'Justice', :air, [], [] ),
+    '12' => MajorAttributes.new(
+      'The Hanged Man', :water, [], [] ),
+    '13' => MajorAttributes.new(
+      'Death', :water, [], [] ),
+    '14' => MajorAttributes.new(
+      'Temperance', :fire, [], [] ),
+    '15' => MajorAttributes.new(
+      'The Devil', :earth, [], [] ),
+    '16' => MajorAttributes.new(
+      'The Tower', :fire, [], [] ),
+    '17' => MajorAttributes.new(
+      'The Star', :air, [], [] ),
+    '18' => MajorAttributes.new(
+      'The Moon', :water, [], [] ),
+    '19' => MajorAttributes.new(
+      'The Sun', :fire, [], [] ),
+    '20' => MajorAttributes.new(
+      'Judgement', :fire, [], [] ),
+    '21' => MajorAttributes.new(
+      'The World', :earth, [], [] )
   }
 
 end
