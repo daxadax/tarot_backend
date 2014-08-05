aa31c9327eda91936c0b1b6e80dba194aa2749ab
Dax <d.dax@email.com>
2014-08-05 19:04:23 +0200

    Deck can be given selected cards

--- lib/tarot/entities/deck.rb
+++ lib/tarot/entities/deck.rb
@@ -3,12 +3,12 @@ module Tarot
     class Deck < Entity
       attr_reader :name
 
-      def initialize
-        @cards  = []
+      def initialize(cards = nil)
+        @cards = set_value_of cards
       end
 
       def deal(number_of_cards = nil)
-        build_new_deck
+        build_new_deck if cards.empty?
 
         return cards.shuffle.pop(number_of_cards) if number_of_cards
         cards
@@ -16,29 +16,45 @@ module Tarot
 
       private
 
+      def build_deck_from(cards)
+        cards.map do |id|
+          build_card id
+        end
+      end
+
       def build_new_deck
         cards.clear
-        build_minor_arcana
-        build_major_arcana
+        build_all_minor_arcana
+        build_all_major_arcana
         cards.flatten
       end
 
-      def build_minor_arcana
+      def build_all_minor_arcana
         suits.each do |suit|
           MINOR_ARCANA.keys.each do |rank|
-            cards << Entities::MinorArcana.new("#{suit}_#{rank}")
+            cards << build_card("#{suit}_#{rank}")
           end
         end
       end
 
-      def build_major_arcana
+      def build_all_major_arcana
         MAJOR_ARCANA.keys.each do |id|
-          cards << Entities::MajorArcana.new(id)
+          cards << build_card(id)
         end
+      end
+
+      def build_card(id)
+        return Entities::MinorArcana.new(id) if id.scan(/^\d/).empty?
+        Entities::MajorArcana.new(id)
       end
 
       def cards
         @cards
+      end
+
+      def set_value_of(cards)
+        return build_deck_from(cards) unless cards.nil?
+        []
       end
 
       def suits
--- spec/entities/deck_spec.rb
+++ spec/entities/deck_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 class DeckSpec < EntitySpec
-  let(:deck) { Entities::Deck.new }
+  let(:cards)   { nil }
+  let(:deck)    { Entities::Deck.new(cards) }
+  let(:result)  { deck.deal }
 
-  describe "dealing" do
+  describe "dealing from a full deck" do
     describe "without a given number of cards" do
-      let(:result) { deck.deal }
-
       it "returns the whole deck" do
         assert_equal 78, result.size
       end
@@ -23,6 +23,44 @@ class DeckSpec < EntitySpec
         refute_same result, result_two
       end
     end
+
+  end
+
+  describe "a given hand" do
+
+    describe "with unknown cards specified" do
+      let(:cards) { ['w_2000', 'wands_5'] }
+
+      it "fails" do
+        assert_failure { result }
+      end
+    end
+
+    describe "with known cards specified" do
+      let(:cards) { ['w_05', '00', 'p_14'] }
+
+      it "returns the specified cards" do
+        assert_equal 3,                   result.count
+        assert_equal known_card_names[0], result[0].display_name
+        assert_equal known_card_names[1], result[1].display_name
+        assert_equal known_card_names[2], result[2].display_name
+      end
+
+      it "can deal a specified subset of the cards" do
+        result = deck.deal(1)
+        assert_equal 1, result.count
+        assert_includes known_card_names, result[0].display_name
+      end
+    end
+
+    def known_card_names
+      [
+        'Five of Wands',
+        'The Fool',
+        'King of Pentacles'
+      ]
+    end
+
   end
 
 end
