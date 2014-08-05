b31e2e8e3a1e2293f5a7598a15abe1135a7a9620
Dax <d.dax@email.com>
2014-08-05 19:04:50 +0200

    Add #average_over_threshold to card_counter

--- spec/services/card_counter_spec.rb
+++ spec/services/card_counter_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 class CardCounterSpec < ServiceSpec
-  let(:cards)         { Entities::Deck.new.deal }
-  let(:card_counter)  { Services::CardCounter.new(cards) }
+  let(:cards)         { nil }
+  let(:deck)          { Entities::Deck.new(cards).deal }
+  let(:card_counter)  { Services::CardCounter.new(deck) }
 
   describe "count" do
     let(:result)        { card_counter.count }
@@ -29,6 +30,27 @@ class CardCounterSpec < ServiceSpec
       assert_equal 18, result.swords
       assert_equal 21, result.court_cards
       assert_kind_of Integer, result.reversed
+    end
+  end
+
+  describe "average_over_threshold" do
+    let(:cards)     { ['w_10', '00', '05'] }
+    let(:result)    { card_counter.average_over_threshold(threshold) }
+
+    describe "when the avg of at least one card type is over the threshold" do
+      let(:threshold) { 25 }
+
+      it "returns true" do
+        assert_equal true, result
+      end
+    end
+
+    describe "when no cardsrd type returns an avg greater than the threshold" do
+      let(:threshold) { 70 }
+
+      it "returns false" do
+        assert_equal false, result
+      end
     end
   end
 
