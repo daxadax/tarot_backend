df5cd5d607047a42fcf661f5eb1c10ec00447272
dax <d.dax@email.com>
2015-06-14 11:45:34 +0200

    Add planetary_influence boundary

--- lib/tarot/services.rb
+++ lib/tarot/services.rb
@@ -2,10 +2,11 @@ require 'tarot/services/service'
 require 'tarot/services/card_counter'
 require 'tarot/services/card_factory'
 require 'tarot/services/moon_info'
+require 'tarot/services/planetary_influence'
 require 'tarot/services/card_boundary'
 require 'tarot/services/moon_boundary'
+require 'tarot/services/planetary_influence_boundary'
 require 'tarot/services/csv_mapper'
-require 'tarot/services/planetary_influence'
 
 module Tarot
   module Services
