328446e9ca6f10aa83d9fd052ac9c36085e18e80
dax <d.dax@email.com>
2018-02-03 16:32:04 +0100

    Remove additional files related to planetary influence

--- lib/tarot/services.rb
+++ lib/tarot/services.rb
@@ -1,11 +1,7 @@
 require 'tarot/services/service'
 require 'tarot/services/card_counter'
 require 'tarot/services/card_factory'
-require 'tarot/services/moon_info'
-require 'tarot/services/planetary_influence'
 require 'tarot/services/card_boundary'
-require 'tarot/services/moon_boundary'
-require 'tarot/services/planetary_influence_boundary'
 require 'tarot/services/csv_mapper'
 
 module Tarot
